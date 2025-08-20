18f61185661051a5619465e786f60ca14677a2bf
Germán De Francesco <germandefrancesco@hotmail.com>
2025-08-20 12:58:21 -0300

    Add ConsoleApp project with OData.Client

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using ConsoleApp.OData.Client;
+using Microsoft.OData.Client;
+using System.Text.Json;
+
+var serviceRoot = new Uri("https://localhost:7007/");
+var ctx = new Container(serviceRoot);
+ctx.Format.UseJson();
+ctx.MergeOption = MergeOption.NoTracking;
+
+var customersQuery1 = ctx.Customers
+    .Expand("Orders($expand=Items)")
+    .Where(c => c.Name == "Customer 1")
+    .Take(50);
+var customers1 = await ((DataServiceQuery<CustomerDto>)customersQuery1).ExecuteAsync();
+Console.WriteLine("Customers using handwritten Expands:");
+Console.WriteLine(JsonSerializer.Serialize(customers1, new JsonSerializerOptions { WriteIndented = true }));
+
+var customersQuery2 = ctx.Customers
+    .Expand(c => c.Orders)
+    .Where(c => c.Name == "Customer 1")
+    .Take(50);
+var customers2 = await ((DataServiceQuery<CustomerDto>)customersQuery2).ExecuteAsync();
+Console.WriteLine("Customers using lambda Expands:");
+Console.WriteLine(JsonSerializer.Serialize(customers2, new JsonSerializerOptions { WriteIndented = true }));
+
+var customersQuery3 = ctx.Customers
+    .Where(c => c.Name == "Customer 1")
+    .Take(50);
+var customers3 = await ((DataServiceQuery<CustomerDto>)customersQuery3).ExecuteAsync();
+Console.WriteLine("Customers without Expands:");
+Console.WriteLine(JsonSerializer.Serialize(customers3, new JsonSerializerOptions { WriteIndented = true }));
---
+++ ConsoleApp/README.md
@@ -0,0 +1,3 @@
+```
+odata-cli --roll-forward LatestMajor generate --metadata-uri "https://localhost:7007/$metadata" --namespace-prefix "ConsoleApp.OData.Client" --outputdir .
+```
