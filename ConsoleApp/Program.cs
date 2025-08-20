8f0c0ba12131554a31402b5caf00f4dc22f2c1f5
Germán De Francesco <germandefrancesco@hotmail.com>
2025-08-20 14:32:20 -0300

    Improve ConsoleApp UI

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,31 +1,39 @@
 ﻿using ConsoleApp.OData.Client;
 using Microsoft.OData.Client;
+using Spectre.Console;
+using Spectre.Console.Json;
+using System.Text;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 
+Console.OutputEncoding = Encoding.UTF8;
 var serviceRoot = new Uri("https://localhost:7007/");
 var ctx = new Container(serviceRoot);
 ctx.Format.UseJson();
 ctx.MergeOption = MergeOption.NoTracking;
 
-var customersQuery1 = ctx.Customers
+var customersWithStringExpand = await ctx.Customers
     .Expand("Orders($expand=Items)")
-    .Where(c => c.Name == "Customer 1")
-    .Take(50);
-var customers1 = await ((DataServiceQuery<CustomerDto>)customersQuery1).ExecuteAsync();
-Console.WriteLine("Customers using handwritten Expands:");
-Console.WriteLine(JsonSerializer.Serialize(customers1, new JsonSerializerOptions { WriteIndented = true }));
+    .ExecuteAsync();
 
-var customersQuery2 = ctx.Customers
+var customersWithLambdaExpand = await ctx.Customers
     .Expand(c => c.Orders)
-    .Where(c => c.Name == "Customer 1")
-    .Take(50);
-var customers2 = await ((DataServiceQuery<CustomerDto>)customersQuery2).ExecuteAsync();
-Console.WriteLine("Customers using lambda Expands:");
-Console.WriteLine(JsonSerializer.Serialize(customers2, new JsonSerializerOptions { WriteIndented = true }));
+    .ExecuteAsync();
 
-var customersQuery3 = ctx.Customers
-    .Where(c => c.Name == "Customer 1")
-    .Take(50);
-var customers3 = await ((DataServiceQuery<CustomerDto>)customersQuery3).ExecuteAsync();
-Console.WriteLine("Customers without Expands:");
-Console.WriteLine(JsonSerializer.Serialize(customers3, new JsonSerializerOptions { WriteIndented = true }));
+var customersWithoutExpand = await ctx.Customers
+    .ExecuteAsync();
+
+WriteJsonInConsole(customersWithStringExpand, "Expand with raw string");
+WriteJsonInConsole(customersWithLambdaExpand, "Expand with lambda");
+WriteJsonInConsole(customersWithoutExpand, "No expand");
+
+static void WriteJsonInConsole<T>(IEnumerable<T> items, string title)
+{
+    AnsiConsole.Write(new Rule($"[black on cyan bold] {Markup.Escape(title)} [/]").RuleStyle("grey"));
+    var json = JsonSerializer.Serialize(items, new JsonSerializerOptions
+    {
+        WriteIndented = true,
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+    });
+    AnsiConsole.Write(new Panel(new JsonText(json)).Expand().Border(BoxBorder.Rounded).BorderColor(Color.CadetBlue));
+}
