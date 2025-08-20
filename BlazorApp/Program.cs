ce0588576e996ddd4939640dd2a10a357898dc57
Germán De Francesco <germandefrancesco@hotmail.com>
2025-08-20 15:12:48 -0300

    Add FluentUI and DataGridODataAdapter

--- BlazorApp/Program.cs
+++ BlazorApp/Program.cs
@@ -1,4 +1,5 @@
 using BlazorApp.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,10 @@ builder.Services.AddScoped(sp =>
     ctx.Configurations.RequestPipeline.OnMessageCreating = args => new Microsoft.OData.Client.HttpClientRequestMessage(args);
     return ctx;
 });
+
+builder.Services.AddHttpClient();
+builder.Services.AddFluentUIComponents();
+builder.Services.AddDataGridODataAdapter();
 
 var app = builder.Build();
 
